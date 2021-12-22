c49f381dd610491c0225340c460d7ff50fc02528
Murielle Lerch <murielle.lerch@dosilab.ch>
2021-12-22 21:15:09 +0100

    added POJOs. Closes #2.

---
+++ src/main/java/moon/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package moon.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/moon/springframework/sfgpetclinic/model/Ver.java
@@ -0,0 +1,5 @@
+package moon.springframework.sfgpetclinic.model;
+
+public class Ver extends Person {
+
+}
